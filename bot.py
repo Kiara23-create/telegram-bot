7dd3253f3c36aca093daaf424cd07396020193cc
Kiara23-create <tatianamansoor74@gmail.com>
2026-06-03 08:44:15 +0200

    clean bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
-[03/06/2026 08:15] Tatiana Msr: python-3.11.9
-[03/06/2026 08:27] Tatiana Msr: import os
+import os
 import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
@@ -23,6 +22,40 @@ async def run():
 if name == "main":
     asyncio.run(run())
 [03/06/2026 08:34] Tatiana Msr: import os
+import requests
+from flask import Flask, request
+
+app = Flask(name)
+
+TOKEN = os.getenv("BOT_TOKEN")
+URL = f"https://api.telegram.org/bot{TOKEN}"
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Bot is running"
+
+@app.route(f"/{TOKEN}", methods=["POST"])
+def webhook():
+    data = request.get_json()
+
+    if "message" in data:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"].get("text", "")
+
+        if text == "/start":
+            send_message(chat_id, "Bonjour ! Le bot fonctionne 🚀")
+
+    return "ok"
+
+def send_message(chat_id, text):
+    requests.post(f"{URL}/sendMessage", json={
+        "chat_id": chat_id,
+        "text": text
+    })
+
+if name == "main":
+    app.run(host="0.0.0.0", port=10000)
+[03/06/2026 08:42] Tatiana Msr: import os
 import requests
 from flask import Flask, request
 
