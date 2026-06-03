1d847dab885b4281584efe6bc9f6c7544c67304b
Kiara23-create <tatianamansoor74@gmail.com>
2026-06-03 07:52:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,18 @@
+import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-import os
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bonjour ! Le bot fonctionne.")
 
-app = Application.builder().token(TOKEN).build()
+def main():
+    app = Application.builder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("start", start))
 
-app.run_polling()
+    app.run_polling()
+
+if name == "main":
+    main()
